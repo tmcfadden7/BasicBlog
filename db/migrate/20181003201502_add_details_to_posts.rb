782c33f9133333f987d1880cd067dafbc859b9b9
Tmcfadden7 <tmcfadden727@gmail.com>
2018-10-03 16:16:53 -0400

    add title, text, user to post table

---
+++ db/migrate/20181003201502_add_details_to_posts.rb
@@ -0,0 +1,7 @@
+class AddDetailsToPosts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :title, :string
+    add_column :posts, :text, :text
+    add_reference :posts, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_180929) do
+ActiveRecord::Schema.define(version: 2018_10_03_201502) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "text"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
