4d69b786b938af017040cde630f907765963310f
Tmcfadden7 <tmcfadden727@gmail.com>
2018-10-12 18:31:59 -0400

    still working on controllers, models, views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @comments = Comment.all
   end
 
   def show
@@ -11,6 +12,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @comment = Comment.new
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_201502) do
+ActiveRecord::Schema.define(version: 2018_10_06_132610) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
